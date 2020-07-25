72877b2625262c547e9987d58e89a7363eb686d9
Kobi <qobyyyy@gmail.com>
2020-07-26 01:16:14 +0300

    WIP

--- src/composed.ts
+++ src/composed.ts
@@ -1,4 +1,4 @@
 import {IRouter} from "express-serve-static-core"
 import {HandleFn} from "./types";
 
-export declare type Composer = (handler: HandleFn) => HandleFn;
+export type Composer = (handler: HandleFn) => HandleFn;
